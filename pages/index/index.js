dc5a045a25932181517a801856d83e76652cc0f8
chengxiaoguang <240184295@qq.com>
2018-02-05 14:08:03 +0800

    修改index

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,9 +19,10 @@ Page({
     scrollTop: 0,
     floorstatus: false,
 
+    showIndex:1,
     hotGoods: [],
     banner: [],
-
+    goodsList:[],
     groupAd:[],
     navList: [],
     groupGoods: [],
@@ -60,46 +61,6 @@ Page({
 
     // 获得首页展示的数据
     that.getIndexData();
-
-    /**if (app.globalData.userInfo) {
-      that.getIndexData();
-      // 页面初始化 options为页面跳转所带来的参数
-      that.setData({
-        id: parseInt(1019000)
-      });
-      that.getCategoryInfo();
-
-      that.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (that.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        that.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          that.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-
-    that.setData({
-      end_time: '2018-02-03'
-    })
-    countdown(that);
-    **/
   },
   /**首页轮播数据 */
   getIndexData: function () {
